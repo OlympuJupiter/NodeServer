aaf30fc52c291cb1987da0d251c4e1a66e81e6eb
Anatoli Morozov <110783383+AnatoliProM@users.noreply.github.com>
2022-11-19 23:00:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(cors());
 
 routes(app);
 
+// Create passport
 // passport.use(new LocalStrategy(
 // 	(username, passport, done) => {
 // 		User.findOne({ username: username }, (err, user) => {
