9382a68461517dd853e08e6835f92aa54a84220c
Jupiter-2002 <103097039+Jupiter-2002@users.noreply.github.com>
2022-11-20 13:53:33 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,12 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
+// login function
 export const login = (req, res, next) => {
 
 }
 
+// register function
 export const register = (req, res, next) => {
     
 }
